7fd37f98850414ccc3903d304cc5ec3656e8f12f
farhanrauf27 <farhanrauf27@gmail.com>
2023-08-10 16:41:27 +0500

    10/8/23

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World2!')
+})
+app.get('/home', (req, res) => {
+  res.sendFile(path.join(__dirname,'templates/index.html'))
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
