5089467173844f7269e160256a8494ed2511b93f
farhanrauf27 <farhanrauf27@gmail.com>
2023-08-11 15:28:03 +0500

    11/8/23

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const express = require('express')
 const path = require('path')
+const multer  = require('multer')
 const app = express()
+const {mergePdfs}  = require('./testpdf')
+const upload = multer({ dest: 'uploads/' })
+app.use('/static', express.static('public'))
+
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World2!')
-})
-app.get('/home', (req, res) => {
   res.sendFile(path.join(__dirname,'templates/index.html'))
 })
+
+
+app.post('/merge', upload.array('pdfs', 12), async (req, res, next) =>{
+    console.log(req.files)
+    let d=await mergePdfs(path.join(__dirname,req.files[0].path),path.join(__dirname,req.files[1].path))
+    res.redirect(`http://localhost:3000/static/${d}.pdf`)
+    // res.send({data:req.files})
+    // req.files is array of `photos` files
+    // req.body will contain the text fields, if there were any
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
